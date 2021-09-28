79a7bd7c8b72408b5d0f72b0ab486c27efabc3af
sanoopsasidharan <87570596+sanoopsasidharan@users.noreply.github.com>
2021-09-28 13:13:48 +0530

    Update validation.js

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -85,18 +85,20 @@ formMessage.addEventListener("keyup",function(e){
 
 
 
-setInterval(()=>{
-   if(checkEmail && checkName && checkNamelength && checkmessage ){
-       button.disabled=false;
-       button.style.background="#5faee3";
-       button.style.color="white"
+document.addEventListener("keyup",function(){
+    if(checkEmail && checkName && checkNamelength && checkmessage ){
+        button.disabled=false;
+        button.style.background="#5faee3";
+        button.style.color="white"
+ 
+    }else{
+     button.disabled=true;
+     button.style.background="grey";
+     button.style.color="#b8b8b8"
+    }
+    
+})
 
-   }else{
-    button.disabled=true;
-    button.style.background="grey";
-    button.style.color="#b8b8b8"
-   }
-}, 100);
 
 
 $("#submit-form").submit((e)=>{
